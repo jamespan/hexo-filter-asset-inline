26cd544f34f4a0d73ad68b2284a91376a3c96a37
panjiabang <panjiabang@gmail.com>
2015-11-19 02:57:43 +0800

    almost done, still need tests

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var cheerio = require('cheerio');
+
+var inline_css = require('./lib/inline-css');
+var inline_js = require('./lib/inline-js');
+
+hexo.extend.filter.register('after_render:html', function(source) {
+  var $ = cheerio.load(source);
+  $('link[rel=stylesheet]').each(inline_css(hexo.config.public_dir, $));
+  $('script').each(inline_js(hexo.config.public_dir, $));
+  return $.html();
+});
