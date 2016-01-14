393a46b5f04641fd562f4098cb83deea53fbf01d
panjiabang <panjiabang@gmail.com>
2016-01-14 22:21:59 +0800

    add enable switch

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var inline_css = require('./lib/inline-css');
 var inline_js = require('./lib/inline-js');
 
 hexo.extend.filter.register('after_render:html', function(source) {
+  if (hexo.config.asset_inline && !hexo.config.asset_inline.enable) {
+    return source;
+  }
   var $ = cheerio.load(source);
   $('link[rel=stylesheet]').each(inline_css(hexo.config.public_dir, $));
   $('script').each(inline_js(hexo.config.public_dir, $));
